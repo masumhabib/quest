46d5c6ff9fc012f566882e103c4820e40d336cbb
Mirza Elahi <me5vp@virginia.edu>
2015-04-14 15:33:03 -0400

    python wrapper functional

--- pylib/potential/pypoissonPot.cpp
+++ pylib/potential/pypoissonPot.cpp
@@ -12,27 +12,40 @@
 /**
  * Python exporters.
  */
-namespace qmicad{
-    namespace python{
-        using namespace potential;
-        
-        /**
-         * poisson potential
-         */
-        //BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(poissonPot_VLR, VLR, 3, 5)
-        void export_poissonPot(){
-            class_<poissonPot, bases<Potential>, shared_ptr<poissonPot>>("poissonPot", init< vec, vec >())
-            .def( init<double, double, double, double>() )
+namespace qmicad {
+namespace python {
+using namespace potential;
+
+/**
+ * poisson potential
+ */
+//BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(poissonPot_VLR, VLR, 3, 5)
+
+void export_poissonPot() {
+    class_<poissonPot, bases<Potential>, shared_ptr < poissonPot >> ("poissonPot", init< vec, vec >())
+            .def(init<double, double, double, double>())
             .def("setMaterialEps", &poissonPot::setMaterialEps)
+            .def("setMaterialni", &poissonPot::setMaterialni)
+            .def("setDoping", &poissonPot::setDoping)
+            .def("setPotentialDirichlet", &poissonPot::setPotentialDirichlet)
+            .def("setPhiByFroce", &poissonPot::setPhiByFroce)
+            .def("generateGrad2LambdaMatrix", &poissonPot::generateGrad2LambdaMatrix)
+            .def("setInitialGuess", &poissonPot::setInitialGuess)
+            .def("calculateLambdaSingleIteration", &poissonPot::calculateLambdaSingleIteration)
+            .def("getRho", &poissonPot::getRho)
+            .def("getPotentialSliceAlongZ", &poissonPot::getPotentialSliceAlongZ)
+            .def("getPotentialSliceAlongX", &poissonPot::getPotentialSliceAlongX)
+            .def("helperDoping", &poissonPot::helperDoping)
+            
             
             //.enable_pickling()
             //        .def("addLinearRegion", &LinearPot::addLinearRegion)
             //        .add_property("NLR", &LinearPot::NLR)
             //        .def("VLR", &LinearPot::VLR, LinearPot_VLR()) 
             ;
-        }
-        
-    }
+}
+
+}
 }
 
 
