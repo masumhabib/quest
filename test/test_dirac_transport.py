93fe9d7e0acf6b27e95c8220b06ecaf24afa4eb7
K M Masum Habib <masum.habib@gmail.com>
2015-04-23 18:12:36 -0400

    Bumped to v0.08.6

--- test/test_dirac_transport.py
+++ test/test_dirac_transport.py
@@ -29,7 +29,7 @@ def simulate(workers):
     tr.verbosity = vprint.MSG_NORMAL
     
     # Simulation parameters ------------------------------------------
-    tr.VERSION_REQUIRED = "0.08.5"
+    tr.VERSION_REQUIRED = "0.08.6"
     tr.verbosity = vprint.MSG_DEBUG 
     # Do a dry run    
     tr.DryRun = False
