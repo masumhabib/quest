3800be7ff4ece92b7024847ef6c479fc0e4425ea
K M Masum Habib <masum.habib@gmail.com>
2015-04-27 16:22:02 -0400

    Semiclassical cpp.

---
+++ simulators/cpp/tmfsc/lib/include/intersect.h
@@ -0,0 +1,18 @@
+/** file: intersect.h
+ * @author K M Masum Habib
+ * Intersection algorithm of two line-segments.
+ * Inspired by: 
+ * http://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+"
+ */
+
+#ifndef INTERSECT_H
+#define INTERSECT_H
+
+namespace tmfsc{ namespace intesect {
+
+
+
+}}
+
+#endif
