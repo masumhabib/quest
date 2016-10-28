9a7726c261d03f23abeedeb1056c99cbb535fa20
K M Masum Habib <masum.habib@gmail.com>
2016-10-27 19:48:43 -0700

    Started logging facility module.

---
+++ logger/include/logger.hpp
@@ -0,0 +1,26 @@
+/**
+ * @brief Logging utility library.
+ *
+ */
+
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+
+namespace logger {
+
+enum Verbosity {
+    Trace = -2,
+    Debug = -1, 
+    Info = 0,
+    Warn = 1,
+    Error = 2,
+};
+
+
+}
+
+#endif
+
+
+
+
