88f511e1dc2726af5c2cb04518120e3b6aced654
K M Masum Habib <masum.habib@gmail.com>
2016-10-27 19:47:51 -0700

    Add sckeletons

--- quester/QuesterApp.cpp
+++ quester/QuesterApp.cpp
@@ -5,12 +5,48 @@
 
 #include "QuesterApp.hpp"
 
+
 namespace quester {
 
 QuesterApp::QuesterApp (int argc, char** argv) {
+    //init (argc, argv);
 }
 
 void QuesterApp::run() {
+}
+
+
+void QuesterApp::parse_args (int argc, char** argv) {
+    //namespace po = boost::program_options;
+
+    //po::options_description generic("General");
+    //generic.add_options()
+    //    ("version",
+    //            "Print version string")
+    //    ("help",        
+    //            "Produce help message")    
+    //    ;
+    //    
+    //bool batch_mode = opts.mode == Options::Mode::Batch;
+    //bool interactive_mode = opts.mode == Options::Mode::Interactive;
+
+    //po::options_description config("Configuration");
+    //config.add_options()
+    //    ("input-file,i", po::value<std::string>(opts.input_file), 
+    //         "Input file name.")
+    //    ("batch,B", po::value<bool>(&batch_mode)->default_value(batch_mode), 
+    //            "Run in batch mode.")
+    //    ("interactive,I", po::value<bool>(&interactive_mode)->default_value(interactive_mode), 
+    //            "Run in interactive mode -- much like MATLAB command mode.")
+    //    ;
+    //
+    //// Hidden options, will be allowed both on command line and
+    //// in config file, but will not be shown to the user.
+    //po::options_description hidden("Hidden options");
+    //hidden.add_options()
+    //    ("long-version", 
+    //            "Print the version info in long, verbose format.")
+    //    ;        
 }
 
 }
--- quester/QuesterApp.hpp
+++ quester/QuesterApp.hpp
@@ -7,14 +7,36 @@
 #ifndef QUESTERAPP_HPP
 #define QUESTERAPP_HPP
 
+#include "boost/program_options.hpp" 
+//#include "logger/logger.hpp"
+
 namespace quester {
+
+struct Options {
+public:
+    //enum class Mode {
+    //    Batch = 0, Interactive;
+    //};
+
+
+    //Mode mode = Mode::Interactive;
+    //std::string input_file;
+
+    //logger::Verbosity verbosity;
+};
 
 class QuesterApp {
 public:
     QuesterApp (int argc, char** argv);
     void run();
+
 private:
     void init ();
+    void parse_args (int argc, char** argv);
+
+
+private:
+    Options opts;
 };
 
 }
