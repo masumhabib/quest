bb34a8955cadeffdc2403939f46e0b21301dea37
K M Masum Habib <masum.habib@gmail.com>
2016-10-30 18:28:29 -0700

    Logger: Add spec

--- logger/include/logger.hpp
+++ logger/include/logger.hpp
@@ -1,5 +1,45 @@
-/**
- * @brief Logging utility library.
+/** @page logger Logging facility API documentation
+ *
+ * @section loggerintro Introduction
+ *
+ * This is a simple loggin facility for QUEST package. It has the following
+ * features:
+ * - Lightweight and fast: 
+ *      - All TRACE level logs are ignored at compile time in Release build. 
+ *      - Logging is either:
+ *          - Asynchronous: uses dedicated thread for speedup.
+ *          - Synchronous: if desired.
+ *      - No file I/O is done by default: logs are written to std::log (stderr). 
+ *        To save log in file, use OS features such as pipeling, ridirection 
+ *        etc. 
+ * - Simple API: 
+ *      - Supports insertion operator <<.
+ *      - Also supports formatted output style syntax like printf ().
+ * - Output is configurable: Any object that has << operator defined can be 
+ *   used as output. Default is std::log.
+ * - In multiprocess (MPI/OpenMP) setup, it only prints logging from the 
+ *   master by default.
+ * - Five predefined log levels: TRACE, DEBUG, INFO, WARN and ERROR.
+ * - Truncate repeating messages after printing predefined number of times.
+ *
+ * @section loggerexamples Examples
+ *
+ * 1. Complete simple example:
+ * @code
+ * #include <logger.hpp>
+ *
+ * void test_logger () {
+ *     //logger::init();
+ *     //using logger::log;
+ *
+ *     //log::info () << "Hey this will appear as information" << log::end;
+ *
+ *     //log::info ("Hey this is formatted info #%d", 0);
+ * }
+ *
+ * @endcode
+ *
+ * 2. For other usage, please see the unit tests: 
  *
  */
 
@@ -11,11 +51,32 @@ namespace logger {
 enum Verbosity {
     Trace = -2,
     Debug = -1, 
-    Info = 0,
-    Warn = 1,
-    Error = 2,
+    Info  =  0,
+    Warn  =  1,
+    Error =  2,
 };
 
+//class Logger {
+//
+//public:
+//    static Logger& get_logger ();
+//    static Logger& create (bool threaded = true, std::size_t mpi_master = 0);
+//private:
+//    static std::unique_ptr<Logger> logger;
+//};
+//
+//void init (bool threaded = true, std::size_t mpi_master = 0) {
+//    Logger::create (threaded, mpi_master);
+//}
+//
+//
+//namespace log {
+//
+//Logger& info () {
+//
+//}
+//
+//}
 
 }
 
