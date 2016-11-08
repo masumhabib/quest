564e4892c49061c997c211573c13332b43dedf64
K M Masum Habib <masum.habib@gmail.com>
2016-11-07 19:34:03 -0800

    QLOG: spec finalized

--- qlog/include/qlog.hpp
+++ qlog/include/qlog.hpp
@@ -1,4 +1,4 @@
-/** @page qlog Logging facility API documentation
+/**@page qlog Logging facility API documentation
  *
  * @section qlogintro Introduction
  *
@@ -6,12 +6,12 @@
  * features:
  * - Lightweight and fast: 
  *      - All TRACE level logs are ignored at compile time in Release build. 
+ *      - Early checking of severity -- no processing is done if the severity
+ *        level is lower than the set value.
  *      - Logging is either:
  *          - Asynchronous: uses dedicated thread for speedup.
  *          - Synchronous: if desired.
  *      - No file I/O is done by default: logs are written to std::log (stderr). 
- *        To save log in file, use OS features such as pipeling, ridirection 
- *        etc. 
  * - Simple API: 
  *      - Supports insertion operator <<.
  *      - Also supports formatted output style syntax like printf ().
@@ -21,25 +21,38 @@
  *   master by default.
  * - Five predefined log levels: TRACE, DEBUG, INFO, WARN and ERROR.
  * - Truncate repeating messages after printing predefined number of times.
+ * - Logging can be turned on/off for a particular class/file/module. 
  *
  * @section qlogexamples Examples
  *
  * 1. Complete simple example:
+ *
  * @code
  * #include <qlog.hpp>
  *
+ * // if logger object is available in the scope, it will be used
+ * // by the LOG macros. Otherwise, the default logger object will be used.
+ * auto logger = qlog::get_logger (_FILE_);
+ *
  * void test_qlog () {
- *     //qlog::init();
- *     //using qlog::log;
+ *     LOG_INFO ("Hey this is formatted info #%d", 0);
+ *     LOG_INFO () << "Hey this is formatted info #" << 0;
  *
- *     //log::info () << "Hey this will appear as information" << log::end;
- *
- *     //log::info ("Hey this is formatted info #%d", 0);
+ *     LOG_DEBUG ("Hey this is formatted info #%d", 0);
+ *     LOG_DEBUG () << "Hey this is formatted info #" << 0;
  * }
  *
  * @endcode
  *
- * 2. For other usage, please see the unit tests: 
+ * Output:
+ * @code
+ *   [II]: Hey this is a formatted info #0 
+ *   [II]: Hey this is a formatted info #0 
+ *   [DD]: [@qlog.hpp:38] Hey this is a formatted info #0 
+ *   [DD]: [@qlog.hpp:39] Hey this is a formatted info #0 
+ * @endcode
+*
+ * 2. For other usage, please see the unit tests.
  *
  */
 
@@ -56,27 +69,15 @@ enum Verbosity {
     Error =  2,
 };
 
-//class qlog {
-//
-//public:
-//    static Logger& get_logger ();
-//    static Logger& create (bool threaded = true, std::size_t mpi_master = 0);
-//private:
-//    static std::unique_ptr<Logger> logger;
-//};
-//
-//void init (bool threaded = true, std::size_t mpi_master = 0) {
-//    Logger::create (threaded, mpi_master);
-//}
-//
-//
-//namespace log {
-//
-//Logger& info () {
-//
-//}
-//
-//}
+enum class Severity {
+    Trace = -2,
+    Debug = -1, 
+    Info  =  0,
+    Warn  =  1,
+    Error =  2,
+};
+
+
 
 }
 
