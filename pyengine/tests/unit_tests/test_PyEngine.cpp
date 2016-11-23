07bc57e90941e28b0ab3da0eb8a0d6a239d07cd6
K M Masum Habib <masum.habib@gmail.com>
2016-11-22 20:04:33 -0800

    QUESTER: add skeliton of unit test for PyEngine class

---
+++ pyengine/tests/unit_tests/test_PyEngine.cpp
@@ -0,0 +1,21 @@
+/** Test cases for Segment class.
+ *
+ */
+
+#include "PyEngine.hpp"
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE RandomTest
+#include <boost/test/unit_test.hpp>
+
+#include <iostream>
+#include <random>
+#include <map>
+#include <string>
+#include <iomanip>
+
+using namespace std;
+
+
+
+
