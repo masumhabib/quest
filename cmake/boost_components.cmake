77ed6adc05eaeb1683fefa9589b6f463ff2b0f07
K M Masum Habib <masum.habib@gmail.com>
2016-12-25 17:48:45 -0800

    Separated third-party build system from quest

---
+++ cmake/boost_components.cmake
@@ -0,0 +1,4 @@
+set (boost_component_list "python;mpi;unit_test_framework;filesystem;program_options;random;serialization;system")
+#set (boost_component_list "mpi;unit_test_framework;filesystem;program_options;random;serialization;system")
+
+
